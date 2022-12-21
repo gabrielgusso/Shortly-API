f80cdb60688f312b9fc4491e490ff9dd9bbd0e0b
gabrielgusso <gabriel.gusso@hotmail.com>
2022-12-21 15:44:43 -0300

    Feat: urls/open route

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -18,3 +18,44 @@ export async function urlController(req, res, next) {
     res.send(err).status(400)
   }
 }
+
+export async function urlIdController(req, res, next) {
+  const id = req.params.id
+
+  try {
+    const shorten = await connection.query(
+      "SELECT * FROM shorten WHERE id=$1",
+      [id]
+    )
+    if (!shorten.rows[0]) {
+      res.sendStatus(404)
+      return
+    }
+
+    res.send(shorten.rows[0])
+
+  } catch (err) {
+    res.send(err).status(400)
+  }
+}
+
+
+export async function urlOpenController(req, res, next) {
+    const shortUrl = req.params.shortUrl
+  
+    try {
+      const shorten = await connection.query(
+        'SELECT * FROM shorten WHERE "shortUrl"=$1',
+        [shortUrl]
+      )
+      if (!shorten.rows[0]) {
+        res.sendStatus(404)
+        return
+      }
+  
+      res.redirect(301, shorten.rows[0].url)
+  
+    } catch (err) {
+      res.send(err).status(400)
+    }
+  }
--- src/routes/url.router.js
+++ src/routes/url.router.js
@@ -1,9 +1,14 @@
 import { Router } from "express"
 import { urlMiddleware } from "../middlewares/url.middleware.js"
-import { urlController } from "../controllers/url.controller.js"
+import { urlController, urlIdController, urlOpenController } from "../controllers/url.controller.js"
 import tokenValidation from "../middlewares/tokenValidation.middleware.js"
 
 export const urlRoute = Router()
 
 urlRoute.post("/urls/shorten", tokenValidation, urlMiddleware, urlController)
+
+urlRoute.get("/urls/:id", urlIdController)
+
+urlRoute.get("/urls/open/:shortUrl", urlOpenController)
+
 
