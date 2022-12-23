471a0aa2b035ac23fce1517fabf2755bc9cf0b6b
gabrielgusso <gabriel.gusso@hotmail.com>
2022-12-23 00:42:03 -0300

    Fix: ranking router LEFT JOIN

--- src/controllers/ranking.controller.js
+++ src/controllers/ranking.controller.js
@@ -4,10 +4,10 @@ export async function rankingController(req, res) {
 
   try {
     const ranking = await connection.query(
-      `SELECT s."userId", u.name, COUNT(s.url) AS "linksCounter", SUM(s."viewsCounter") AS "visitCount" 
-      FROM shorten s JOIN users u ON s."userId" = u.id
-      GROUP BY u.name, s."userId"
-      ORDER BY "visitCount" DESC LIMIT 10`
+      `SELECT u.id, u.name, COUNT(s.url) AS "linksCounter", SUM(s."viewsCounter") AS "visitCount" 
+      FROM users u LEFT JOIN shorten s ON s."userId" = u.id
+      GROUP BY u.id, s."userId"
+      ORDER BY "visitCount" DESC NULLS LAST LIMIT 10`
     )
     
     res.send(ranking.rows)
